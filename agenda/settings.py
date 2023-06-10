629d34de03620445896405cc347d65dbdcb40b3d
Henrique Jose Alves Gonzaga <98667584+henriquealvesgonzaga87@users.noreply.github.com>
2023-06-10 15:14:53 +0100

    Update settings.py

--- agenda/settings.py
+++ agenda/settings.py
@@ -86,15 +86,8 @@ WSGI_APPLICATION = 'agenda.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
 
 DATABASES = {}
-
 DATABASES['default'] = dj_database_url.config()
 
 # Password validation
